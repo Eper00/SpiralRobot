08fecb1de608dc49d8af50c4d81b4680fb1beaef
Epres Tamás <104941550+Eper00@users.noreply.github.com>
2026-05-22 11:49:52 +0200

    Add files via upload

--- toy/il/training.py
+++ toy/il/training.py
@@ -7,37 +7,161 @@ from imitation.data import rollout
 from imitation.data.wrappers import RolloutInfoWrapper
 from imitation.policies.serialize import load_policy
 from imitation.util.util import make_vec_env
+import typer
+import yaml
+from pathlib import Path
+from datetime import datetime
+import numpy as np
+import torch
 from stable_baselines3.common.policies import ActorCriticPolicy
+from imitation.util import logger as imit_logger
+app = typer.Typer()
+def load_config(path: str):
+    with open(path, "r") as f:
+        return yaml.safe_load(f)
 
 
-rng = np.random.default_rng(0)
-env = make_vec_env(
-    "seals:seals/CartPole-v0",
-    rng=rng,
-    n_envs=1,
-    post_wrappers=[lambda env, _: RolloutInfoWrapper(env)],  # for computing rollouts
-)
-expert = load_policy(
-    "ppo-huggingface",
-    organization="HumanCompatibleAI",
-    env_name="seals-CartPole-v0",
-    venv=env,
-)
-rollouts = rollout.rollout(
-    expert,
-    env,
-    rollout.make_sample_until(min_timesteps=None, min_episodes=24),
-    rng=rng,
-)
-transitions = rollout.flatten_trajectories(rollouts)
+@app.command()
+def train(config_path: str):
 
-bc_trainer = bc.BC(
-    observation_space=env.observation_space,
-    action_space=env.action_space,
-    demonstrations=transitions,
-    rng=rng,
-)
-bc_trainer.train(n_epochs=1)
-reward, _ = evaluate_policy(bc_trainer.policy, env, 10)
-print("Reward:", reward)
-torch.save(bc_trainer.policy.state_dict(), "bc_policy.pt")
+    # -------------------------
+    # load config
+    # -------------------------
+    cfg = load_config(config_path)
+
+    env_cfg = cfg["env"]
+   
+    env_id = env_cfg["env_id"]
+    policy_cfg = cfg["policy"]
+    seed = env_cfg["seed"]
+    n_envs = env_cfg["n_training_envs"]
+    seed = env_cfg["seed"]
+    bc_cfg = cfg["bc"]
+    n_expert_episodes = bc_cfg["n_expert_episodes"]
+    n_epochs = bc_cfg["n_epochs"]
+    n_eval_envs = env_cfg["n_eval_envs"]
+   
+    net_arch = policy_cfg["net_arch"]
+    lr = float(policy_cfg["learning_rate"])
+    activation_fn = policy_cfg["activation_fn"]
+    if activation_fn == "relu":
+        activation_fn = torch.nn.ReLU
+    elif activation_fn == "tanh":
+        activation_fn = torch.nn.Tanh
+    else:
+        raise ValueError(f"Unsupported activation function: {activation_fn}")
+    # -------------------------
+    # run folders
+    # -------------------------
+    rng = np.random.default_rng(seed)
+    run_name = datetime.now().strftime("run_%Y%m%d_%H%M%S_bc"+f"_{env_id}")
+    root = Path("results") / run_name
+    model_dir = root / "models"
+    log_dir = root / "logs"
+
+    model_dir.mkdir(parents=True, exist_ok=True)
+    log_dir.mkdir(parents=True, exist_ok=True)
+
+    # -------------------------
+    # env
+    # -------------------------
+    env = make_vec_env(
+        "seals:seals/"+env_id,
+        n_envs=n_envs,
+        rng=rng,
+        post_wrappers=[
+            lambda env, _: RolloutInfoWrapper(env)
+        ]
+    )
+
+    # -------------------------
+    # load expert
+    # -------------------------
+    expert = load_policy(
+        "ppo-huggingface",
+        organization="HumanCompatibleAI",
+        env_name="seals-"+env_id,
+        venv=env,
+    )
+
+    # -------------------------
+    # collect demonstrations
+    # -------------------------
+    rollouts = rollout.rollout(
+        expert,
+        env,
+        rollout.make_sample_until(
+            min_timesteps=None,
+            min_episodes=n_expert_episodes
+        ),
+        rng=rng,
+    )
+
+    transitions = rollout.flatten_trajectories(
+        rollouts
+    )
+
+    # -------------------------
+    # BC trainer
+    # -------------------------
+
+    logger = imit_logger.configure(
+        folder=str(log_dir),
+        format_strs=["tensorboard", "stdout"],
+    )
+    policy = ActorCriticPolicy(
+        observation_space=env.observation_space,
+        action_space=env.action_space,
+        lr_schedule=lambda _: lr,
+        net_arch=net_arch,
+        activation_fn=activation_fn
+    )
+    bc_trainer = bc.BC(
+        observation_space=env.observation_space,
+        action_space=env.action_space,
+        demonstrations=transitions,
+        rng=rng,
+        policy=policy,  
+        custom_logger=logger
+
+ 
+    )
+
+    # -------------------------
+    # train BC
+    # -------------------------
+    bc_trainer.train(
+        n_epochs=n_epochs,
+        log_rollouts_venv=env,
+        log_rollouts_n_episodes=5,
+        
+    )
+
+    # -------------------------
+    # evaluate
+    # -------------------------
+        # -------------------------
+    # eval callback
+    # -------------------------
+
+    reward, _ = evaluate_policy(
+        bc_trainer.policy,
+        env,
+        n_eval_episodes=n_eval_envs
+    )
+
+    print(f"Reward: {reward}")
+
+    # -------------------------
+    # save weights
+    # -------------------------
+    torch.save(
+        bc_trainer.policy.state_dict(),
+        model_dir / "bc_policy.pt"
+    )
+
+    print(f"Done. Results saved to: {root}")
+
+
+if __name__ == "__main__":
+    app()
--- toy/rl/training.py
+++ toy/rl/training.py
@@ -1,15 +1,13 @@
 import typer
 import yaml
+import torch
+
 from pathlib import Path
 from datetime import datetime
 
 from stable_baselines3 import PPO
-from imitation.algorithms.bc import BC
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback
-from stable_baselines3.common.save_util import load_from_zip_file
-import torch
-
 
 
 app = typer.Typer()
@@ -24,26 +22,29 @@ def load_config(path: str):
 def train(config_path: str):
 
     # -------------------------
-    # load config
+    # CONFIG
     # -------------------------
     cfg = load_config(config_path)
 
     env_cfg = cfg["env"]
-    train_cfg = cfg["train"]
-
+    rl_cfg = cfg["rl"]
+    policy_cfg = cfg["policy"]
+    warm_start = env_cfg["warm_start"]
     env_id = env_cfg["env_id"]
-    n_training_envs = env_cfg["n_training_envs"]
+    n_train_envs = env_cfg["n_training_envs"]
     n_eval_envs = env_cfg["n_eval_envs"]
     seed = env_cfg["seed"]
 
-    total_timesteps = train_cfg["total_timesteps"]
-    eval_freq = train_cfg["eval_freq"]
-    n_eval_episodes = train_cfg["n_eval_episodes"]
+    total_timesteps = rl_cfg["total_timesteps"]
+    eval_freq = rl_cfg["eval_freq"]
+    n_eval_episodes = rl_cfg["n_eval_episodes"]
+
+    net_arch = policy_cfg["net_arch"]
 
     # -------------------------
-    # run folders
+    # RUN DIRS
     # -------------------------
-    run_name = datetime.now().strftime("run_%Y%m%d_%H%M%S_rl")
+    run_name = datetime.now().strftime("run_%Y%m%d_%H%M%S_rl"+f"_{env_id}")
     root = Path("results") / run_name
     model_dir = root / "models"
     log_dir = root / "logs"
@@ -52,49 +53,69 @@ def train(config_path: str):
     log_dir.mkdir(parents=True, exist_ok=True)
 
     # -------------------------
-    # envs
+    # ENVS
     # -------------------------
-    train_env = make_vec_env(env_id, n_envs=n_training_envs, seed=seed)
+    train_env = make_vec_env(
+        env_id,
+        n_envs=n_train_envs,
+        seed=seed
+    )
 
-    eval_env = make_vec_env(env_id, n_envs=n_eval_envs, seed=seed)
+    eval_env = make_vec_env(
+        env_id,
+        n_envs=n_eval_envs,
+        seed=seed
+    )
 
     # -------------------------
-    # eval callback
+    # EVAL CALLBACK
     # -------------------------
     eval_callback = EvalCallback(
         eval_env,
         best_model_save_path=str(model_dir / "best"),
         log_path=str(log_dir),
-        eval_freq=max(500 // n_training_envs, 1),
+        eval_freq=eval_freq,
         n_eval_episodes=n_eval_episodes,
         deterministic=True,
-        render=False,
     )
 
     # -------------------------
-    # PPO model
+    # PPO MODEL
     # -------------------------
     model = PPO(
-        "MlpPolicy",
-        train_env,
-        policy_kwargs=dict(net_arch=[32, 32]),
+        policy="MlpPolicy",
+        env=train_env,
+        policy_kwargs=dict(
+            net_arch=net_arch,
+        ),
         tensorboard_log=str(log_dir),
         verbose=1,
         seed=seed,
     )
 
-    state_dict = torch.load("rl/bc_policy.pt", map_location="cpu")
+    # -------------------------
+    # LOAD BC WEIGHTS (optional warm start)
+    # -------------------------
+   
+    bc_path = Path(cfg["rl"]["bc_path"])
 
-    # súlyok átmásolása
-    model.policy.load_state_dict(state_dict, strict=False)
+    if warm_start and bc_path.exists():
+        state_dict = torch.load(bc_path, map_location="cpu")
+        model.policy.load_state_dict(state_dict, strict=False)
+        print(f"Loaded BC from {bc_path}")
+    else:
+        print("No BC weights found. Training from scratch.")
 
-    # RL training
+    # -------------------------
+    # TRAIN PPO
+    # -------------------------
     model.learn(
         total_timesteps=total_timesteps,
         callback=eval_callback,
     )
+
     # -------------------------
-    # save final model
+    # SAVE FINAL MODEL
     # -------------------------
     model.save(str(model_dir / "final_model"))
 
